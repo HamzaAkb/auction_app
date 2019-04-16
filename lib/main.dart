fa84dfcc282782538c451af37232d51debf31808
HamzaAkb <hamzawakbar@gmail.com>
2019-04-16 13:52:19 +0500

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,89 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
+        drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            // Header
+            new UserAccountsDrawerHeader(
+              accountName: Text('Hamza Akbar'),
+              accountEmail: Text('hamzawakbar@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                color: Colors.redAccent,
+              ),
+            ),
+            // Body
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home, color: Colors.red),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Account'),
+                leading: Icon(Icons.person, color: Colors.red),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Current Bids'),
+                leading: Icon(Icons.shopping_basket, color: Colors.red),
+              ),
+            ),
+            InkWell(
+              // onTap: () {
+              //   Navigator.push(context,
+              //       MaterialPageRoute(builder: (context) => new Cart()));
+              // },
+              child: ListTile(
+                title: Text('History'),
+                leading: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Log Out'),
+                leading: Icon(Icons.favorite, color: Colors.red),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(
+                  Icons.help,
+                  color: Colors.green,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       )
     );
   }
